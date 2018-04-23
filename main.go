e7b5ebffa3d074eef3ec9cb9b0ad1072e777c11a
Niclas Geiger <niclas.geiger@netlight.com>
2018-04-23 15:11:45 +0200

    added artifacts

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
 
+	"time"
+
+	"github.com/golang/glog"
+	niclasgeigerclient "github.com/niclasgeiger/crd-controller/client/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	kuberconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	master      = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+)
+
+func main() {
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(*master, *kuberconfig)
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %v", err)
+	}
+
+	client, err := niclasgeigerclient.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building example clientset: %v", err)
+	}
+
+	list, err := client.ExampleV1().Foos("default").List(metav1.ListOptions{})
+	if err != nil {
+		glog.Fatalf("Error listing all Foos: %v", err)
+	}
+
+	for _, db := range list.Items {
+		fmt.Printf("Foo %s with Bar %q\n", db.Name, db.Spec.Bar)
+	}
+	time.Sleep(30 * time.Second)
 }
